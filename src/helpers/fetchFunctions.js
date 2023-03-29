f484b25b2c04dea65e66da1b690f32985634f251
Eduardo Klein <eduardo.klein@live.com>
2023-03-29 10:06:59 -0300

    Feat: função fetchProductsList finalizada

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (query) => {
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return new Error('Termo de busca não informado');
+  }
 };
