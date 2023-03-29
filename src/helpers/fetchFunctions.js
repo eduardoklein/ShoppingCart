6910adad44f0f4061afda3a78c5cd7db25a013e0
Eduardo Klein <eduardo.klein@live.com>
2023-03-29 15:24:47 -0300

    Feat: site mostrando produtos

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,10 +3,12 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = async (query) => {
+  if (!query) {
+    return new Error('Termo de busca não informado');
+  }
   try {
     const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-    const data = await response.json();
-    return data;
+    return response;
   } catch (error) {
     return new Error('Termo de busca não informado');
   }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const products = document.querySelector('.products');
 
-// Mudança para commit inicial
+const getData = async () => {
+  const response = await fetchProductsList('computador');
+  const data = await response.json();
+  return data;
+};
+
+const productList = async () => {
+  const resolve = await getData();
+  const data = Object.values(resolve);
+  const arrayOfProducts = data[4];
+  return arrayOfProducts;
+};
+
+const elementCreate = async () => {
+  const arrayOfProducts = await productList();
+  arrayOfProducts.forEach((element) => {
+    const obj = {
+      id: element.id,
+      title: element.title,
+      thumbnail: element.thumbnail,
+      price: element.price,
+    };
+    const section = createProductElement(obj);
+    products.appendChild(section);
+  });
+};
+
+window.addEventListener('load', elementCreate);
