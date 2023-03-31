215cf037ca61410e9a9e39a9e1fe185cd33fe1be
Eduardo Klein <eduardo.klein@live.com>
2023-03-31 11:19:22 -0300

    Feat: adicionando elemento de erro se promise da API der algum erro

--- src/main.js
+++ src/main.js
@@ -6,35 +6,37 @@ import './style.css';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const products = document.querySelector('.products');
 const loading = document.createElement('span');
-console.log(loading);
 
 const loadingCreate = () => {
   loading.className = 'loading';
   loading.innerHTML = 'carregando...';
   products.appendChild(loading);
-  console.log(loading);
 };
 
 const loadingRemove = () => loading.remove();
 
 const elementCreate = async () => {
-  const data = await fetchProductsList('computador');
-  loadingRemove();
-  data.forEach((element) => {
-    const obj = {
-      id: element.id,
-      title: element.title,
-      thumbnail: element.thumbnail,
-      price: element.price,
-    };
-    const section = createProductElement(obj);
-    products.appendChild(section);
-  });
+  try {
+    const data = await fetchProductsList('computador');
+    loadingRemove();
+    data.forEach((element) => {
+      const obj = {
+        id: element.id,
+        title: element.title,
+        thumbnail: element.thumbnail,
+        price: element.price,
+      };
+      const section = createProductElement(obj);
+      products.appendChild(section);
+    });
+  } catch (error) {
+    loadingRemove();
+    const errorElement = document.createElement('span');
+    errorElement.className = 'error';
+    errorElement.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+    products.appendChild(errorElement);
+  }
 };
-
-window.addEventListener('load', elementCreate);
-
-window.addEventListener('click', loadingCreate);
 
 window.addEventListener('load', () => {
   loadingCreate();
